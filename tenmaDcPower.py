f51fcc272b22c351c3239f40d89555150074c8e5
kxtells <jordi.kstells@gmail.com>
2017-09-04 16:27:52 +0200

    tenma set maximum current

--- tenmaDcPower.py
+++ tenmaDcPower.py
@@ -11,6 +11,9 @@ class tenma:
             parity=serial.PARITY_NONE,
             stopbits=serial.STOPBITS_ONE)
 
+        self.NCHANNELS = 4
+        self.MAX_MA = 5100
+
 
     def __sendCommand(self, command):
         self.ser.write(command)
@@ -26,9 +29,40 @@ class tenma:
         self.__sendCommand("*IDN?")
         print self.__readOutput()
 
+    def setCurrent(self, channel, mA):
+        """
+            Returns True if current was set correctly, False otherwise
+        """
+        if channel > self.NCHANNELS:
+            return False
+
+        if mA > self.MAX_MA:
+            return False
+
+        command = "ISET{channel}:{amperes:.3f}"
+
+        A = float(mA) / 1000.0
+        command = command.format(channel=1, amperes=A)
+
+        commandCheck = "ISET{channel}?".format(channel=1)
+
+        self.__sendCommand(command)
+
+        self.__sendCommand(commandCheck)
+        readcurrent = float(self.__readOutput())
+
+        if readcurrent * 1000 != mA:
+            print "Incorrect current read"
+            return False
+
+        return True
+
+
+
     def close(self):
         self.ser.close()
 
 T = tenma('/dev/ttyUSB0')
-T.printVersion()
+#T.printVersion()
+T.setCurrent(1, 3500)
 T.close()
